b0df106f79921b29be568218d8a09a0bda1bb01b
Jeremy Guan <32561489+jeremyguan1@users.noreply.github.com>
2020-09-08 23:56:39 -0700

    Initial commit

---
+++ whatsapp-mern/src/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Chart.css"
+
+function Chat() {
+    return (
+        <div className="chart">
+            <h1>im chart</h1>
+        </div>
+    )
+}
+
+export default Chat
