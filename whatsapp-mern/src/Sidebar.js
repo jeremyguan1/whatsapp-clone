627957b5c99c999810abe22612658ca3f1d4249a
JeremyGuan <jeremyguan1@gmail.com>
2020-09-09 00:05:34 -0700

    Initial

--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -4,9 +4,8 @@ import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
 import { SearchOutlined } from '@material-ui/icons'
-
-
 import { IconButton, Avatar } from '@material-ui/core'
+import SidebarChat from './SidebarChat'
 
 function Sidebar() {
     return (
@@ -30,6 +29,10 @@ function Sidebar() {
                     <SearchOutlined />
                     <input type="text" placeholder="Search or start new chat"/>
                 </div>
+            </div>
+
+            <div className="sidebar__chats">
+                <SidebarChat />
             </div>
         </div>
     )
---
+++ whatsapp-mern/src/SidebarChat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Sidebar.css'
+
+function SidebarChat() {
+    return (
+        <div className="sidebarChat">
+            123
+        </div>
+    )
+}
+
+export default SidebarChat
